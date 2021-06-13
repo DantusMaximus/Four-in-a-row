d251285dd1a40aae1ac92bc48660ef79bee91cd8
DantusMaximus <dante.krang@outlook.com>
2021-06-13 17:34:49 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FourInARow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            
+        }
+    }
+}
