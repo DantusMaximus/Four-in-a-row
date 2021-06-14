f033c00aa3cde62af521824c1d24334c93fd56cf
DantusMaximus <dante.krang@outlook.com>
2021-06-14 10:04:51 +0200

    UI improvement

--- Print.cs
+++ Print.cs
@@ -22,6 +22,7 @@ static class Print
             Console.WriteLine();
             Console.WriteLine("───────────────");
         }
+        Console.WriteLine(" 1 2 3 4 5 6 7");
     }
     public static void Prompt(string message)
     {
